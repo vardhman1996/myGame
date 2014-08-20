59a24d7e2d060efe3750dd420c8488c5f106d6d3
vardhman1996 <vardhman.mehta96@gmail.com>
2014-08-20 15:59:13 +0530

    Update myGame.js

--- myGame.js
+++ myGame.js
@@ -39,6 +39,58 @@ var compare = function(choice1, choice2) {
             return "scissors wins";
         }
     }
+    else {
+    	return "You have selected the wrong option";
+    }
 };
 
 console.log(compare (userChoice,computerChoice));
+
+while ( compare (userChoice,computerChoice) === "The result is a tie!") {
+    userChoice = prompt("Do you choose rock, paper or scissors?");
+    var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+} console.log("Computer: " + computerChoice);
+
+var compare = function(choice1, choice2) {
+    if(choice1 === choice2) {
+        return "The result is a tie!";
+    }
+    
+    else if (choice1==="rock") {
+        if (choice2 === "scissors") {
+            return "rock wins";
+        }
+        else {
+            return "paper wins";
+        }
+    }
+    
+    else if (choice1 === "paper") {
+        if (choice2 === "rock") {
+            return "paper wins";
+        }
+        else {
+            return "scissors wins";
+        }
+    }
+    
+    else if (choice1 ==="scissors") {
+        if (choice2 === "rock") {
+            return "rock wins";
+        }
+        else {
+            return "scissors wins";
+        }
+    }
+    else {
+        return "You have entered a wrong option";
+    }
+};
+console.log(compare (userChoice,computerChoice));
+};
